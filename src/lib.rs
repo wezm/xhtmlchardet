781347a145768a00dc0fab6197ce1b23d15d2cf6
Wesley Moore <wes@wezm.net>
2015-06-01 21:34:11 +1000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,167 @@
+use std::io::{Read,Write};
+use std::ascii::AsciiExt;
+
+struct Bom ( u8, u8, u8, u8 );
+
+#[derive(Clone)]
+enum Flavour {
+    UCS,
+    UTF,
+    EBCDIC,
+    ASCII,
+    Unknown,
+}
+
+#[derive(Clone)]
+enum ByteOrder {
+    BigEndian,
+    LittleEndian,
+    Unusual2143,
+    Unusual3412,
+    NotApplicable
+}
+
+#[derive(Clone)]
+enum Width {
+    EightBit     = 8,
+    SixteenBit   = 16,
+    ThirtyTwoBit = 32
+}
+
+#[derive(Clone)]
+struct Descriptor (
+    Flavour,
+    Width,
+    ByteOrder,
+);
+
+// 32-Bit Encodings
+const UCS_4_BE: Descriptor = Descriptor(Flavour::UCS, Width::ThirtyTwoBit, ByteOrder::BigEndian);
+const UCS_4_LE: Descriptor = Descriptor(Flavour::UCS, Width::ThirtyTwoBit, ByteOrder::LittleEndian);
+const UCS_4_2143: Descriptor = Descriptor(Flavour::UCS, Width::ThirtyTwoBit, ByteOrder::Unusual2143);
+const UCS_4_3412: Descriptor = Descriptor(Flavour::UCS, Width::ThirtyTwoBit, ByteOrder::Unusual3412);
+
+// 16-Bit Encodings
+const UTF_16_BE: Descriptor = Descriptor(Flavour::UTF, Width::SixteenBit, ByteOrder::BigEndian);
+const UTF_16_LE: Descriptor = Descriptor(Flavour::UTF, Width::SixteenBit, ByteOrder::LittleEndian);
+
+const UTF_8: Descriptor = Descriptor(Flavour::UTF, Width::EightBit, ByteOrder::NotApplicable);
+const EBCDIC: Descriptor = Descriptor(Flavour::EBCDIC, Width::EightBit, ByteOrder::NotApplicable);
+
+// ASCII compatible encodings
+const ASCII_32BIT_BE: Descriptor = Descriptor(Flavour::Unknown, Width::ThirtyTwoBit, ByteOrder::BigEndian);
+const ASCII_32BIT_LE: Descriptor = Descriptor(Flavour::Unknown, Width::ThirtyTwoBit, ByteOrder::LittleEndian);
+const ASCII_16BIT_BE: Descriptor = Descriptor(Flavour::Unknown, Width::SixteenBit, ByteOrder::BigEndian);
+const ASCII_16BIT_LE: Descriptor = Descriptor(Flavour::Unknown, Width::SixteenBit, ByteOrder::LittleEndian);
+const ASCII_8BIT: Descriptor = Descriptor(Flavour::ASCII, Width::EightBit, ByteOrder::NotApplicable);
+
+pub fn detect<R: Read>(reader: &mut R, hint: Option<String>) -> Option<String> {
+    // Read the first 4 bytes and see if they help
+    let mut first_four_bytes = [0u8; 4];
+    match reader.read(&mut first_four_bytes) {
+        Ok(bytes_read) => assert_eq!(bytes_read, 4),
+        Err(e) => panic!(e)
+    };
+
+    let bom = Bom(
+        first_four_bytes[0],
+        first_four_bytes[1],
+        first_four_bytes[2],
+        first_four_bytes[3]
+    );
+
+    // http://www.w3.org/TR/2004/REC-xml-20040204/#sec-guessing-no-ext-info
+    // Can do below without the Bom type if slice pattern syntax becomes non-experimental
+    // let possible = match first_four_bytes {
+    //     // With Byte Order Mark
+    //     [0x00, 0x00, 0xFE, 0xFF]             => Some("UCS-4BE"),
+    //     [0xFF, 0xFE, 0x00, 0x00]             => Some("UCS-4LE"),
+    //     [0x00, 0x00, 0xFF, 0xFE]             => Some("UCS-4OE"),
+    //     [0xFE, 0xFF, 0x00, 0x00]             => Some("UCS-4EO"),
+    //     [0xFE, 0xFF, c, d] if c > 0 && d > 0 => Some("UTF-16BE"),
+    //     [0xFF, 0xFE, c, d] if c > 0 && d > 0 => Some("UCS-16LE"),
+    //     [0xEF, 0xBB, 0xBF, _   ]             => Some("UTF-8"),
+
+    //     //  Without Byte Order Mark
+    //     [0x00, 0x00, 0x00, 0x3C] |
+    //     [0x3C, 0x00, 0x00, 0x00] |
+    //     [0x00, 0x00, 0x3C, 0x00] |
+    //     [0x00, 0x3C, 0x00, 0x00]             => Some("32-bit"),
+    //     [0x00, 0x3C, 0x00, 0x3F]             => Some("16-bit Big Endian"),
+    //     [0x3C, 0x00, 0x3F, 0x00]             => Some("16-bit Little Endian"),
+    //     [0x3C, 0x3F, 0x78, 0x6D]             => Some("8-bit"),
+    //     [0x4C, 0x6F, 0xA7, 0x94]             => Some("EBCDIC"),
+    //     // This may be UTF-8 without an encoding declaration as this is not required
+    //     // for UTF-8
+    //     _                                    => Some("Other"),
+    // };
+
+    let possible = match bom {
+        // With Byte Order Mark
+        Bom(0x00, 0x00, 0xFE, 0xFF)                   => Some(UCS_4_BE),
+        Bom(0xFF, 0xFE, 0x00, 0x00)                   => Some(UCS_4_LE),
+        Bom(0x00, 0x00, 0xFF, 0xFE)                   => Some(UCS_4_2143),
+        Bom(0xFE, 0xFF, 0x00, 0x00)                   => Some(UCS_4_3412),
+        Bom(0xFE, 0xFF, c   , d   ) if c > 0 && d > 0 => Some(UTF_16_BE),
+        Bom(0xFF, 0xFE, c   , d   ) if c > 0 && d > 0 => Some(UTF_16_LE),
+        Bom(0xEF, 0xBB, 0xBF, _   )                   => Some(UTF_8),
+
+        //  Without Byte Order Mark
+        Bom(0x00, 0x00, 0x00, 0x3C)                   => Some(ASCII_32BIT_BE),
+        Bom(0x3C, 0x00, 0x00, 0x00)                   => Some(ASCII_32BIT_LE),
+        Bom(0x00, 0x00, 0x3C, 0x00)                   => Some(Descriptor(Flavour::Unknown, Width::ThirtyTwoBit, ByteOrder::Unusual2143)),
+        Bom(0x00, 0x3C, 0x00, 0x00)                   => Some(Descriptor(Flavour::Unknown, Width::ThirtyTwoBit, ByteOrder::Unusual3412)),
+        Bom(0x00, 0x3C, 0x00, 0x3F)                   => Some(ASCII_32BIT_BE),
+        Bom(0x3C, 0x00, 0x3F, 0x00)                   => Some(ASCII_16BIT_LE),
+        Bom(0x3C, 0x3F, 0x78, 0x6D)                   => Some(ASCII_8BIT),
+        Bom(0x4C, 0x6F, 0xA7, 0x94)                   => Some(EBCDIC),
+        // This may be UTF-8 without an encoding declaration as this is not required
+        // for UTF-8
+        _                                             => None,
+    };
+
+    // Now that byte size may have been determined try reading the first 512ish bytes to read an
+    // encoding declaration
+    let mut buf = [0u8; 512];
+    let bytes_read = reader.read(&mut buf).unwrap();
+
+    // Look for encoding="", charset="?"?
+    search("encoding=", &buf.to_vec(), possible.clone())
+        .or_else(|| search("charset=", &buf.to_vec(), possible.clone()))
+        .map(|encoding| encoding.to_ascii_lowercase())
+}
+
+fn search(needle: &str, haystack: &Vec<u8>, descriptor: Option<Descriptor>) -> Option<String> {
+    let Descriptor(ref flavour, ref width, ref order) = descriptor.unwrap_or(ASCII_8BIT);
+    let chunk_size = (width.clone() as usize) / 8;
+    let needle_bytes = needle.as_bytes();
+
+    let mut log = std::io::stderr();
+
+    let mut index = match *order {
+        ByteOrder::NotApplicable | ByteOrder::LittleEndian  => 0,
+        ByteOrder::BigEndian => chunk_size - 1,
+        ByteOrder::Unusual2143 => 2,
+        ByteOrder::Unusual3412 => 1,
+    };
+
+    let mut ascii_bytes = Vec::with_capacity(haystack.len() / chunk_size);
+    while index < haystack.len() {
+        ascii_bytes.push(haystack[index]);
+        index += chunk_size;
+    }
+
+    let ascii_haystack = String::from_utf8_lossy(&ascii_bytes);
+    // log.write(format!("want {} in {}\n", needle, ascii_haystack).as_bytes());
+
+    ascii_haystack.find(needle).map(|pos| {
+        ascii_haystack.chars()
+            .skip(pos + needle.len())
+            .skip_while(|char| *char == '"')
+            .take_while(|char| *char != '"').collect()
+    })
+}
+
+// #[test]
+// fn it_works() {
+// }
---
+++ tests/lib.rs
@@ -0,0 +1,58 @@
+extern crate toml;
+extern crate xhtmlchardet;
+
+use std::io;
+
+use std::io::{Read,Write};
+use std::fs::File;
+use std::collections::BTreeMap;
+
+fn read_config() -> toml::Value {
+    // let path = Path::new(&config_arg);
+    let mut file = File::open("tests/fixtures.toml")
+        .ok()
+        .expect("Unable to open tests/fixtures.toml");
+    let mut toml = String::new();
+    file.read_to_string(&mut toml).ok().expect("Error reading config file");
+    let config: toml::Value = toml.parse().ok().expect("Error parsing config file");
+    config
+}
+
+#[test]
+fn test_fixtures() {
+    let config = read_config();
+
+    let tests = config.lookup("fixtures")
+        .expect("no fixtures in file")
+        .as_slice().expect("fixtures is not an array");
+
+    // Map the filename to the output charset
+    let mut expected: BTreeMap<String, Option<String>> = BTreeMap::new();
+    let mut actual: BTreeMap<String, Option<String>> = BTreeMap::new();
+
+    for value in tests {
+        let test = value.as_table().expect("invalid test");
+        let expected_charset = test["charset"].as_str().unwrap();
+        let variant = test["variant"].as_str().unwrap();
+        let path = format!("tests/{}-{}.txt", expected_charset, variant);
+
+        expected.insert(path.to_string(), Some(expected_charset.to_string()));
+
+        let mut file = File::open(&path).ok().expect(&format!("Unable to open {}", path));
+        let actual_charset = xhtmlchardet::detect(&mut file, None);
+        actual.insert(path.to_string(), actual_charset);
+    }
+
+    // Verify the results
+    let mut passed = true;
+    let mut f = std::io::stderr();
+    for (test, result) in expected.iter() {
+        if *result != actual[test] {
+            f.write(format!("{}: {:?} != {:?}\n", test, result, actual[test]).as_bytes());
+            passed = false;
+        }
+    }
+
+    assert!(passed);
+}
+
